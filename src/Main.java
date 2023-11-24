7e84df56661c99424d8b0f987a7eac1fc684c2bb
Samak01 <125607243+Samak01@users.noreply.github.com>
2023-11-24 05:54:41 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,128 +1,155 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.TreeMap;
 
-class Main {
-    private static final Map<String, Integer> ROMAN_NUMERALS = new HashMap<>();
-
-    static {
-        ROMAN_NUMERALS.put("I", 1);
-        ROMAN_NUMERALS.put("II", 2);
-        ROMAN_NUMERALS.put("III", 3);
-        ROMAN_NUMERALS.put("IV", 4);
-        ROMAN_NUMERALS.put("V", 5);
-        ROMAN_NUMERALS.put("VI", 6);
-        ROMAN_NUMERALS.put("VII", 7);
-        ROMAN_NUMERALS.put("VIII", 8);
-        ROMAN_NUMERALS.put("IX", 9);
-        ROMAN_NUMERALS.put("X", 10);
-        ROMAN_NUMERALS.put("XI", 11);
-        ROMAN_NUMERALS.put("XII", 12);
-        ROMAN_NUMERALS.put("XIII", 13);
-        ROMAN_NUMERALS.put("XIV", 14);
-        ROMAN_NUMERALS.put("XV", 15);
-        ROMAN_NUMERALS.put("XVI", 16);
-        ROMAN_NUMERALS.put("XVII", 17);
-        ROMAN_NUMERALS.put("XVIII", 18);
-        ROMAN_NUMERALS.put("XIX", 19);
-        ROMAN_NUMERALS.put("XX", 20);
-
-    }
-
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите выражение: ");
+        String expression = scanner.nextLine();
 
-        while (true) {
-            try {
-                System.out.println("Введите выражение через пробель (a + b): ");
-                String input = scanner.nextLine();
+        String result = calc(expression);
+        System.out.println(result);
+    }
 
+    public static String calc(String input) {
+        Converter converter = new Converter();
+        String[] actions = {"+", "-", "*", "/"};
+        String[] regActions = {"\\+", "-", "\\*", "/"};
 
-                String result = calc(input);
-                System.out.println("Результь: " + result);
-            } catch (IllegalArgumentException e) { // Java.lang.IllegalArgumentException — это исключение, которое возникает в языке программирования Java, когда переданный аргумент методу или конструктору является недопустимым или некорректным. Такое исключение может возникнуть, когда аргумент имеет неправильное значение, неверный формат или не отвечает ожидаемым требованиям.
-                System.out.println("Ошибка: " + e.getMessage());
+        int actionInd = -1;
+        for (int i = 0; i < actions.length; i++){
+            if (input.contains(actions[i])){
+                actionInd = i;
                 break;
             }
         }
 
-        System.out.println("Программа завершена.");
-    }
 
-    static String calc(String input) {
-        String[] tokens = input.split("\\s+");
-        boolean isRoman = isRomanNumber(tokens[0]) && isRomanNumber(tokens[2]);
-        int num1 = parseNumber(tokens[0], isRoman);
-        int num2 = parseNumber(tokens[2], isRoman);
-        String operator = tokens[1];
-        int result;
-
-        if ((num1 < 1 || num1 > 10 || num2 < 1 || num2 > 10) && !isRoman) {
-            throw new IllegalArgumentException("Допустимы только числа от 1 до 10 включительно.");
+        if (actionInd == -1){
+            return "Неподдерживаемая операция";
         }
 
-        if ((num1 >= 1 && num1 <= 10 && num2 >= 1 && num2 <= 10) && isRoman) {
-            throw new IllegalArgumentException("Нельзя выполнять операции между арабскими и римскими числами одновременно.");
+        String[] data = input.split(regActions[actionInd]);
+
+        if (data.length > 2){
+            return "Не вводите больше двух чисел!!";
+
         }
-
-        switch (operator) {
-            case "+":
-                result = num1 + num2;
-                break;
-            case "-":
-                result = num1 - num2;
-                break;
-            case "*":
-                result = num1 * num2;
-                break;
-            case "/":
-                if (num2 != 0) {
-                    result = num1 / num2; // целочисленное деление
-                } else {
-                    throw new IllegalArgumentException("Ошибка: деление на ноль");
-                }
-                break;
-            default:
-                throw new IllegalArgumentException("Неподдерживаемая операция");
-        }
-
-        if (isRoman && result <= 0) {
-            throw new IllegalArgumentException("Результат работы с римскими числами должен быть положительным.");
-        }
-
-        return isRoman ? toRoman(result) : String.valueOf(result);
-    }
-
-    private static boolean isRomanNumber(String input) {
-        return ROMAN_NUMERALS.containsKey(input.toUpperCase());
-    }
-
-    private static int parseNumber(String input, boolean isRoman) {
-        try {
-            if (isRoman) {
-                return ROMAN_NUMERALS.get(input.toUpperCase()); //Метод Java String toUpperCase() преобразует строку в верхний регистр.
+        if (converter.isRoman(data[0]) == converter.isRoman(data[1])){
+            int a, b;
+            boolean isRoman = converter.isRoman(data[0]);
+            if (isRoman){
+                a = converter.romanToInt(data[0]);
+                b = converter.romanToInt(data[1]);
             } else {
-                int number = Integer.parseInt(input);
-                if (number < 1 || number > 10) {
-                    throw new IllegalArgumentException("Допустимы только числа от 1 до 10 включительно.");
-                }
-                return number;
+                a = Integer.parseInt(data[0]);
+                b = Integer.parseInt(data[1]);
             }
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Не удалось распознать число: " + input);
+
+            if ((a <= 1 || a >= 10 || b <= 1 || b >= 10) ){
+                if (isRoman){
+                    return "Вводите числа от I до X!";
+                } else {
+                    return "Вводите числа от 1 до 10!";
+                }
+            }
+
+            int result = 0;
+            switch (actions[actionInd]){
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                case "/":
+                    if (b == 0){
+                        return "Ошибка: деление на ноль";
+                    }
+                    result = a / b;
+                    break;
+                default:
+            }
+            if (isRoman && result < 0){
+                return "Результат римских чисел дольжен быть положительным!!";
+            }
+            return isRoman ? converter.intToRoman(result) : String.valueOf(result);
+        } else {
+            return "Вводите числа в одном формате";
         }
     }
 
-    private static String toRoman(int number) {
-        if (number < 1 || number > 10) {
-            throw new IllegalArgumentException("Допустимы только числа от 1 до 10 включительно.");
+    // Класс Converter здесь остается без изменений
+    public static class Converter {
+        TreeMap<Character, Integer> romanKeyMap = new TreeMap<>();
+        TreeMap<Integer, String> arabianKeyMap = new TreeMap<>();
+
+        public Converter() {
+            romanKeyMap.put('I', 1);
+            romanKeyMap.put('V', 5);
+            romanKeyMap.put('X', 10);
+            romanKeyMap.put('L', 50);
+            romanKeyMap.put('C', 100);
+            romanKeyMap.put('D', 500);
+            romanKeyMap.put('M', 1000);
+
+            arabianKeyMap.put(1000, "M");
+            arabianKeyMap.put(900, "CM");
+            arabianKeyMap.put(500, "D");
+            arabianKeyMap.put(400, "CD");
+            arabianKeyMap.put(100, "C");
+            arabianKeyMap.put(90, "XC");
+            arabianKeyMap.put(50, "L");
+            arabianKeyMap.put(40, "XL");
+            arabianKeyMap.put(10, "X");
+            arabianKeyMap.put(9, "IX");
+            arabianKeyMap.put(5, "V");
+            arabianKeyMap.put(4, "IV");
+            arabianKeyMap.put(1, "I");
+
         }
 
-        StringBuilder result = new StringBuilder();
-        for (Map.Entry<String, Integer> entry : ROMAN_NUMERALS.entrySet()) {
-            while (number >= entry.getValue()) {
-                result.append(entry.getKey());
-                number -= entry.getValue();
-            }
+
+        public boolean isRoman(String number) {
+            return romanKeyMap.containsKey(number.charAt(0));
         }
-        return result.toString();
+
+        public String intToRoman(int number) {
+            String roman = "";
+            int arabianKey;
+            do {
+                arabianKey = arabianKeyMap.floorKey(number);
+                roman += arabianKeyMap.get(arabianKey);
+                number -= arabianKey;
+            } while (number != 0);
+            return roman;
+
+
+        }
+
+
+        public int romanToInt(String s) {
+            int end = s.length() - 1;
+            char[] arr = s.toCharArray();
+            int arabian;
+            int result = romanKeyMap.get(arr[end]);
+            for (int i = end - 1; i >= 0; i--) {
+                arabian = romanKeyMap.get(arr[i]);
+
+                if (arabian < romanKeyMap.get(arr[i + 1])) {
+                    result -= arabian;
+                } else {
+                    result += arabian;
+                }
+
+
+            }
+            return result;
+
+        }
+
     }
 }
